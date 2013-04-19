e1c32c32d236c507a58fd06dec4ab1a7d548c232
daithi-coombes <webeire@gmail.com>
2013-04-19 08:50:48 +0100

    init pr commit

--- phonegap/assets/www/js/githubNotifier.js
+++ phonegap/assets/www/js/githubNotifier.js
@@ -5,13 +5,21 @@ function get_git_user(){
 	var url_foo = 'https://api.github.com/user/repos'
 	//url = url_foo;
 	var username = 'daithi-coombes';
-	var password = '*************'
+	var password = '***'
 	var data = false;
 
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4 && xhr.status==200){
-			document.getElementById('debug').innerHTML = "status: " + xhr.status + "\n" + xhr.responseText
+			res = JSON.parse(xhr.responseText);
+			stdout = "";
+
+			for(var i=0; i<res.length; i++)
+				stdout += res[i].subject.title + "\n";
+
+			document.getElementById('debug').innerHTML = "status: " + xhr.status + "\n" + stdout;
+			if(navigator.notification)
+				navigator.notification.beep();
 		}
 	}
 	xhr.dataType = 'json';
